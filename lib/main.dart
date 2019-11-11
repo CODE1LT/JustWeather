3a08356cc2faac825506b3f56106354b7ef7f87a
Aurelijus Suslavicius <a.suslavicius@ba.lt>
2019-11-11 11:21:06 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:io' show Platform;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:weather_app/models/src/app_settings.dart';
+import 'package:weather_app/page/add_city_page.dart';
+import 'package:weather_app/page/page_container.dart';
+import 'package:weather_app/styles.dart';
+import 'package:flutter/foundation.dart'
+    show debugDefaultTargetPlatformOverride;
+
+void main() {
+  _setTargetPlatformForDesktop();
+  WidgetsFlutterBinding.ensureInitialized();
+  AppSettings settings = new AppSettings();
+
+  // Don't allow landscape mode
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(new MyApp(settings: settings)));
+}
+
+void _setTargetPlatformForDesktop() {
+  TargetPlatform targetPlatform;
+  if (Platform.isMacOS) {
+    targetPlatform = TargetPlatform.iOS;
+  } else if (Platform.isLinux || Platform.isWindows) {
+    targetPlatform = TargetPlatform.android;
+  }
+  if (targetPlatform != null) {
+    debugDefaultTargetPlatformOverride = targetPlatform;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  final AppSettings settings;
+
+  const MyApp({Key key, this.settings}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = ThemeData(
+      fontFamily: "Cabin",
+      primaryColor: AppColor.midnightSky,
+      accentColor: AppColor.midnightCloud,
+      primaryTextTheme: Theme.of(context).textTheme.apply(
+            bodyColor: AppColor.textColorDark,
+            displayColor: AppColor.textColorDark,
+          ),
+      textTheme: Theme.of(context).textTheme.apply(
+            bodyColor: AppColor.textColorDark,
+            displayColor: AppColor.textColorDark,
+          ),
+    );
+
+    return MaterialApp(
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: PageContainer(settings: settings),
+    );
+  }
+}
